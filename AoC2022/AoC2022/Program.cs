f3af44aebe6aa161f1434e12536f6530d5013298
Mattias Makra <mattias@makra.org>
2022-12-01 10:39:59 +0000

    Fix so that it runs in codespace.

--- AoC2022/AoC2022/Program.cs
+++ AoC2022/AoC2022/Program.cs
@@ -1,10 +1,13 @@
 using AoC2022.One;
 
 Console.WriteLine("Day one");
-const string dayOneDataFilePath = "/Users/mazk0/Developer/AdventOfCode/Data2022/DayOneData.txt";
+const string dayOneDataFilePath = "/workspaces/AdventOfCode/Data2022/DayOneData.txt";
 
 Console.WriteLine("OneOne");
 Console.WriteLine(OneOne.GetMaxTotalCalories(dayOneDataFilePath));
 
+Console.WriteLine("---");
 Console.WriteLine("OneTwo");
 Console.WriteLine(OneTwo.GetMaxTotalCaloriesForTopThree(dayOneDataFilePath));
+
+Console.WriteLine("-----");

9074ce2ed5c52854f42d338a0edec385dab58cf2
Mattias Makra <mattias@makra.org>
2022-12-06 14:01:54 +0000

    Fix naming of variable.

--- AoC2022/AoC2022/Six/SixOne.cs
+++ AoC2022/AoC2022/Six/SixOne.cs
@@ -2,15 +2,15 @@ namespace AoC2022.Six;
 
 public static class SixOne
 {
-    public static int GetMessageStartIndex(string dataFilepath, int makerLength)
+    public static int GetMessageStartIndex(string dataFilepath, int markerLength)
     {
         var row = File.ReadLines(dataFilepath).Single();
         for (var index = 0; index < row.Length; index++)
         {
-            var stringToValidate = row.Substring(index, makerLength);
-            if (stringToValidate.Distinct().Count() == makerLength)
+            var stringToValidate = row.Substring(index, markerLength);
+            if (stringToValidate.Distinct().Count() == markerLength)
             {
-                return index + makerLength;
+                return index + markerLength;
             }
         }
 

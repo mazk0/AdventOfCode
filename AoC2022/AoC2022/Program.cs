7bec7dbb4027f883f21f89329362848839ad5583
Mattias Makra <mattias@makra.org>
2022-12-06 10:53:39 +0000

    Fix incorrectly named output. Take name from class name.

--- AoC2022/AoC2022/Program.cs
+++ AoC2022/AoC2022/Program.cs
@@ -8,61 +8,61 @@ using AoC2022.Two;
 Console.WriteLine("Day one");
 const string dayOneDataFilePath = "/workspaces/AdventOfCode/Data2022/DayOneData.txt";
 
-Console.WriteLine("OneOne");
+Console.WriteLine(nameof(OneOne));
 Console.WriteLine(OneOne.GetMaxTotalCalories(dayOneDataFilePath));
-
 Console.WriteLine("---");
-Console.WriteLine("OneTwo");
+Console.WriteLine(nameof(OneTwo));
 Console.WriteLine(OneTwo.GetMaxTotalCaloriesForTopThree(dayOneDataFilePath));
 
 Console.WriteLine("-----");
+
 Console.WriteLine("Day two");
 const string dayTwoDataFilePath = "/workspaces/AdventOfCode/Data2022/DayTwoData.txt";
 
-Console.WriteLine("TwoOne");
+Console.WriteLine(nameof(TwoOne));
 Console.WriteLine(TwoOne.SimulateTournament(dayTwoDataFilePath));
-
 Console.WriteLine("---");
-Console.WriteLine("TwoOne");
+Console.WriteLine(nameof(TwoTwo));
 Console.WriteLine(TwoTwo.SimulateTournament(dayTwoDataFilePath));
 
 Console.WriteLine("-----");
+
 Console.WriteLine("Day three");
 const string dayThreeDataFilePath = "/workspaces/AdventOfCode/Data2022/DayThreeData.txt";
 
-Console.WriteLine("ThreeOne");
+Console.WriteLine(nameof(ThreeOne));
 Console.WriteLine(ThreeOne.GetPrioritySum(dayThreeDataFilePath));
-
 Console.WriteLine("---");
-Console.WriteLine("ThreeTwo");
+Console.WriteLine(nameof(ThreeTwo));
 Console.WriteLine(ThreeTwo.GetPrioritySum(dayThreeDataFilePath));
 
 Console.WriteLine("-----");
+
 Console.WriteLine("Day four");
 const string dayFourDataFilePath = "/workspaces/AdventOfCode/Data2022/DayFourData.txt";
 
-Console.WriteLine("FourOne");
+Console.WriteLine(nameof(FourOne));
 Console.WriteLine(FourOne.GetOverlappingRangeCount(dayFourDataFilePath));
-
 Console.WriteLine("---");
-Console.WriteLine("FourTwo");
+Console.WriteLine(nameof(FourTwo));
 Console.WriteLine(FourTwo.GetOverlappingRangeCount(dayFourDataFilePath));
 
 Console.WriteLine("-----");
+
 Console.WriteLine("Day five");
 const string dayFiveDataFilePath = "/workspaces/AdventOfCode/Data2022/DayFiveData.txt";
 
-Console.WriteLine("FiveOne");
+Console.WriteLine(nameof(FiveOne));
 Console.WriteLine(FiveOne.GetSupplyStacks(dayFiveDataFilePath));
 Console.WriteLine("---");
-Console.WriteLine("FiveTwo");
+Console.WriteLine(nameof(FiveTwo));
 Console.WriteLine(FiveTwo.GetSupplyStacks(dayFiveDataFilePath));
 
 Console.WriteLine("-----");
 Console.WriteLine("Day six");
 const string daySixDataFilePath = "/workspaces/AdventOfCode/Data2022/DaySixData.txt";
 
-Console.WriteLine("SixOne");
+Console.WriteLine(nameof(SixOne));
 Console.WriteLine(SixOne.GetMessageStartIndex(daySixDataFilePath, 4));
 Console.WriteLine("---");
 Console.WriteLine("SixTwo");

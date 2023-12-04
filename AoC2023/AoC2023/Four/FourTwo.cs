bcc0a79b30b5715e4ad862c85b8e79f1ff06e8be
Mattias Makra <mattias@makra.org>
2023-12-04 21:40:40 +0100

    Part two Day four.

--- AoC2023/AoC2023/Four/FourTwo.cs
+++ AoC2023/AoC2023/Four/FourTwo.cs
@@ -4,12 +4,29 @@ public static class FourTwo
 {
     public static int Run(string dataFilepath)
     {
-        var maxValue = 0;
-        foreach (var dataRow in File.ReadLines(dataFilepath))
+        var data = File.ReadAllLines(dataFilepath);
+        var cardsCount = new Dictionary<int, int>();
+        for (var i = 1; i < data.Length + 1; i++)
         {
-           
+            cardsCount.Add(i, 1);
         }
+        
+        foreach (var dataRow in data)
+        {
+            var card = dataRow.Split(": ");
+            var gameId = int.Parse(card[0].Split(" ").Last());
+            var game = card[1].Split(" | ");
+            var winning = game[0].Split(" ");
+            var row = game[1].Split(" ");
 
-        return maxValue;
+            var count = row.Where(x => winning.Contains(x) && x != "").Count();
+
+            for (var i = 0; i < count; i++)
+            {
+                cardsCount[gameId + i + 1] += cardsCount[gameId];
+            }
+        }
+        
+        return cardsCount.Values.Sum();;
     }
 }
